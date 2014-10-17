cab80f2de04b864970c0d55226283d657640d82a
mo-om <chengwei915@gmail.com>
2014-10-17 15:45:34 +0800

    Update util.js
    format

--- js/util.js
+++ js/util.js
@@ -12,7 +12,7 @@
 			 * 模拟生成guid
 			 * @return {String} 返回模拟生成的guid
 			 */
-			guid: function(){
+			guid: function() {
 				return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 					var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 					return v.toString(16);
